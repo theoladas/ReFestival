98d4f5fcfe2ba0fd42298efb3914a8913689930a
Theo <theodorosladasmail@gmail.com>
2021-09-21 16:21:16 +0100

    update

--- app.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navUL = document.querySelector(".nav-list2");
-
-hamburger.addEventListener("click", () => {
-  navUL.classList.toggle("show");
-});
